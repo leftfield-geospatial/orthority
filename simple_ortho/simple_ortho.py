639c9e16c6d05ecbca376b342df6fa3ca8835ae3
Dugal Harris <dugalh@users.noreply.github.com>
2021-05-03 13:30:56 +0200

    added docstrings

--- simple_ortho/simple_ortho.py
+++ simple_ortho/simple_ortho.py
@@ -11,6 +11,24 @@ logger = get_logger(__name__)
 
 class Camera():
     def __init__(self, focal_len, sensor_size, im_size, geo_transform, position, orientation):
+        """
+        Camera class to project from camera 2D pixel co-ordinates to world 3D (x,y,z) co-ordinates, and vice-versa
+
+        Parameters
+        ----------
+        focal_len :     float
+                        focal length in mm
+        sensor_size :   numpy.array_like
+                        sensor (ccd) [width, height] in mm
+        im_size :       numpy.array_like
+                        image [width, height]] in pixels
+        geo_transform :     numpy.array_like
+                            gdal or rasterio 6 element image transform
+        position :      numpy.array_like
+                        column vector of [x=easting, y=northing, z=altitude] camera location co-ordinates, in image CRS
+        orientation :   numpy.array_like
+                        camera orientation [omega, phi, kappa] angles in degrees
+        """
 
         self.update_extrinsic(position, orientation)
 
@@ -23,7 +41,18 @@ class Camera():
 
         self.update_intrinsic(geo_transform)
 
+
     def update_extrinsic(self, position, orientation):
+        """
+        Update camera extrinsic parameters
+
+        Parameters
+        ----------
+        position :      numpy.array_like
+                        column vector of [x=easting, y=northing, z=altitude] camera location co-ordinates, in image CRS
+        orientation :   numpy.array_like
+                        camera orientation [omega, phi, kappa] angles in degrees
+        """
         if len(position) != 3 or len(orientation) != 3:
             raise Exception('len(position) != 3 or len(orientation) != 3')
         self._T = position.copy()
@@ -47,10 +76,20 @@ class Camera():
         self._R = np.dot(np.dot(omega_r, phi_r), kappa_r)
         return
 
-
     def update_intrinsic(self, geo_transform, kappa=None):
+        """
+        Update camera instrinsic parameters
 
-        # See https://support.pix4d.com/hc/en-us/articles/202559089-How-are-the-Internal-and-External-Camera-Parameters-defined
+        Parameters
+        ----------
+        geo_transform : numpy.array_like
+                        gdal or rasterio 6 element image transform
+        kappa :         float
+                        (optional) kappa angle in degrees - if not specified kappa from last call of update_extrinsic()
+                        is used
+        """
+
+        # Adapted from https://support.pix4d.com/hc/en-us/articles/202559089-How-are-the-Internal-and-External-Camera-Parameters-defined
         # and https://en.wikipedia.org/wiki/Camera_resectioning
         if len(geo_transform) != 6:
             raise Exception('len(geo_transform) != 6')
@@ -69,24 +108,54 @@ class Camera():
         return
 
     def unproject(self, X):
-        # Unproject from 3D world to 2D image co-ordinates
+        """
+        Unproject from 3D world co-ordinates to 2D image co-ordinates
+
+        Parameters
+        ----------
+        X : numpy.array_like
+            3-by-N array of 3D world (x=easting, y=northing, z=altitude) co-ordinates to unproject.
+            (x,y,z) along the first dimension.
+
+        Returns
+        -------
+        ij : numpy.array_like
+             2-by-N array of image (i=row, j=column) co-ordinates. (i, j) along the first dimension.
+        """
         # x,y,z down 1st dimension
         if not(X.shape[0] == 3 and X.shape[1] > 0):
             raise Exception('not(X.shape[0] == 3 and X.shape[1] > 0')
+
         # reshape/transpose to xyz along 1st dimension, and broadcast rotation and translation for each xyz vector
         X_ = np.dot(self._R.T, (X - self._T))
         # homogenise xyz/z and apply intrinsic matrix, discarding 3rd dimension
         ij = np.dot(self._K, X_/X_[2, :])[:2, :]
+
         return ij
 
     def project_to_z(self, ij, Z):
-        # Reproject from 2D image to 3D world co-ordinates with known Z
-        # x,y,z down 1st dimension
+        """
+        Project from 2D image co-ordinates to 3D world co-ordinates at a specified Z
+
+        Parameters
+        ----------
+        ij : numpy.array_like
+             2-by-N array of image (i=row, j=column) co-ordinates. (i, j) along the first dimension.
+        Z : numpy.array_like
+            1-by-N array of Z (altitude) values to project to
+
+        Returns
+        -------
+        X : numpy.array_like
+            3-by-N array of 3D world (x=easting, y=northing, z=altitude) co-ordinates.
+            (x,y,z) along the first dimension.
+        """
         if not(ij.shape[0] == 2 and ij.shape[1] > 0):
             raise Exception('not(ij.shape[0] == 2 and ij.shape[1] > 0)')
-        # TODO: check Z dimensions
+
         ij_ = np.row_stack([ij, np.ones((1, ij.shape[1]))])
         X_ = np.dot(np.linalg.inv(self._K), ij_)
         X_R = np.dot(self._R, X_) # rotate first (camera to world)
         X = (X_R * (Z - self._T[2])/X_R[2,:]) + self._T  # scale to desired Z and offset to world
+
         return X
