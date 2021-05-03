f963f43c93fb573d864a08a812c61b12edf5f7d9
Dugal Harris <dugalh@users.noreply.github.com>
2021-05-03 16:59:16 +0200

    started class to orthorectify preliminary script for testing simple_ortho module

---
+++ scripts/ortho_im.py
@@ -0,0 +1,45 @@
+import cv2
+import rasterio as rio
+from rasterio.warp import calculate_default_transform, reproject, Resampling,  transform
+import numpy as np
+import pandas as pd
+import pathlib
+from simple_ortho import get_logger
+from simple_ortho import simple_ortho
+import datetime
+from scripts import root_path
+import yaml
+
+logger = get_logger(__name__)
+np.set_printoptions(precision=3)
+np.set_printoptions(suppress=True)
+
+im_filename = pathlib.Path(r"V:\Data\NGI\UnRectified\3318D_2016_1143\3318D_2016_1143_07_0298_RGB_PRE.tif")
+# im_filename = pathlib.Path(r"V:\Data\NGI\UnRectified\3318D_2016_1143\3318D_2016_1143_08_0321_RGB_PRE.tif")
+# im_filename = pathlib.Path(r"V:\Data\NGI\UnRectified\3318D_2016_1143\3318D_2016_1143_11_0451_RGB_PRE.tif")
+dem_filename = pathlib.Path(r"D:\Data\Development\Projects\PhD GeoInformatics\Data\CGA\SUDEM\SUDEM_3318B_D_5m.tif")
+ext_filename = pathlib.Path(r"C:\Data\Development\Projects\PhD GeoInformatics\Docs\PCI\NGI Orthorectification\extori3318D_2016_1143_lo19wgs84n_e_rect.txt")
+
+# TODO require cmd line spec of dem, raw, and ext_ori filenames, optional out file and res, overwrite in cfg if they are specified
+# also, make a default config file?
+with open(root_path.joinpath('config.yml'), 'r') as f:
+    config = yaml.safe_load(f)
+
+with rio.open(im_filename) as raw_im:
+    geo_transform = raw_im.transform
+    im_size = np.float64([raw_im.width, raw_im.height])
+
+cam_extorid = pd.read_csv(ext_filename, header=None, sep=' ', index_col=0,
+                   names=['file', 'easting', 'northing', 'altitude', 'omega', 'phi', 'kappa'])
+
+im_filestem = pathlib.Path(im_filename).stem[:-4]
+im_ext = cam_extorid.loc[im_filestem]
+
+orientation = np.pi * im_ext[['omega', 'phi', 'kappa']] / 180.
+position = np.array([im_ext['easting'], im_ext['northing'], im_ext['altitude']])
+
+camera_config = config['camera']
+camera = simple_ortho.Camera(camera_config['focal_len'], camera_config['sensor_size'], camera_config['im_size'],
+                             geo_transform, position, orientation)
+
+ortho_im = simple_ortho.OrthoIm(im_filename, dem_filename, camera, config=config['ortho'])
--- simple_ortho/simple_ortho.py
+++ simple_ortho/simple_ortho.py
@@ -1,5 +1,6 @@
 from simple_ortho import get_logger
 import numpy as np
+import os
 import cv2
 import rasterio as rio
 from rasterio.warp import calculate_default_transform, reproject, Resampling,  transform
@@ -32,7 +33,7 @@ class Camera():
 
         self.update_extrinsic(position, orientation)
 
-        if len(sensor_size) != 2 or len(im_size) != 2:
+        if np.size(sensor_size) != 2 or np.size(im_size) != 2:
             raise Exception('len(sensor_size) != 2 or len(image_size) != 2')
 
         self._sensor_size = sensor_size
@@ -49,13 +50,13 @@ class Camera():
         Parameters
         ----------
         position :      numpy.array_like
-                        column vector of [x=easting, y=northing, z=altitude] camera location co-ordinates, in image CRS
+                        list of [x=easting, y=northing, z=altitude] camera location co-ordinates, in image CRS
         orientation :   numpy.array_like
                         camera orientation [omega, phi, kappa] angles in degrees
         """
-        if len(position) != 3 or len(orientation) != 3:
+        if np.size(position) != 3 or np.size(orientation) != 3:
             raise Exception('len(position) != 3 or len(orientation) != 3')
-        self._T = position.copy()
+        self._T = np.array(position).reshape(3, 1)
 
         self._omega, self._phi, self._kappa = orientation
 
@@ -91,15 +92,15 @@ class Camera():
 
         # Adapted from https://support.pix4d.com/hc/en-us/articles/202559089-How-are-the-Internal-and-External-Camera-Parameters-defined
         # and https://en.wikipedia.org/wiki/Camera_resectioning
-        if len(geo_transform) != 6:
-            raise Exception('len(geo_transform) != 6')
+        if np.size(geo_transform) < 6:
+            raise Exception('len(geo_transform) < 6')
 
         if kappa is None:
             kappa = self._kappa
 
         # image signed dimensions for orientation (origin and kappa)
         image_size_s = -np.sign(np.cos(kappa)) * np.float64(
-            [np.sign(geo_transform[0]) * self._im_size[0], np.sign(geo_transform[4]) * self._im_size[0]])
+            [np.sign(geo_transform[0]) * self._im_size[0], np.sign(geo_transform[4]) * self._im_size[1]])
         sigma_xy = self._focal_len * image_size_s / self._sensor_size   # x,y signed focal lengths in pixels
 
         self._K = np.array([[sigma_xy[0], 0, self._im_size[0] / 2],
@@ -141,8 +142,8 @@ class Camera():
         ----------
         ij : numpy.array_like
              2-by-N array of image (i=row, j=column) co-ordinates. (i, j) along the first dimension.
-        Z : numpy.array_like
-            1-by-N array of Z (altitude) values to project to
+        Z :  numpy.array_like
+             1-by-N array of Z (altitude) values to project to
 
         Returns
         -------
@@ -156,6 +157,143 @@ class Camera():
         ij_ = np.row_stack([ij, np.ones((1, ij.shape[1]))])
         X_ = np.dot(np.linalg.inv(self._K), ij_)
         X_R = np.dot(self._R, X_) # rotate first (camera to world)
-        X = (X_R * (Z - self._T[2])/X_R[2,:]) + self._T  # scale to desired Z and offset to world
+        X = (X_R * (Z - self._T[2])/X_R[2, :]) + self._T  # scale to desired Z and offset to world
 
         return X
+
+class OrthoIm():
+    def __init__(self, raw_im_filename, dem_filename, camera, config=None, ortho_im_filename=None, ):
+        """
+        Class to orthorectify image with known DEM and camera model
+
+        Parameters
+        ----------
+        raw_im_filename :   str
+                            Filename of raw image to orthorectified
+        dem_filename :      str
+                            Filename of DEM covering raw image
+        camera :            simple_orth.Camera
+                            camera object relevant to raw image
+        ortho_im_filename : str
+                            (optional) specify the filename of the orthorectified image to create.  If not specified,
+                            appends '_ORTHO' to the raw_im_filename
+        config :            dict
+                            (optional) dictionary of configuration parameters.  With key, value pairs as follows:
+                                'dem_interp': Interpolation type for resampling DEM (average, bilinear, cubic,
+                                              cubic_spline, gauss, lanczos) default = 'cubic_spline'
+                                'ortho_interp': Interpolation type for ortho-image (average, bilinear, cubic, lanczos,
+                                                nearest), default = 'bilinear'
+                                'resolution':  Output pixel size [x, y] in m, default = [0.5, 0.5]
+                                'compression': GeoTIFF compression type (deflate, jpeg, jpeg2000, lzw, zstd, none),
+                                                default = 'deflate'
+                                'tile_size': default = Tile/block [x, y] size in pixels, default = [512, 512]
+        """
+        if not os.path.exists(raw_im_filename):
+            raise Exception(f"Raw image file {raw_im_filename} does not exist")
+
+        if not os.path.exists(dem_filename):
+            raise Exception(f"DEM file {dem_filename} does not exist")
+
+        self._raw_im_filename = pathlib.Path(raw_im_filename)
+        self._dem_filename = pathlib.Path(dem_filename)
+
+        self._camera = camera
+
+        if ortho_im_filename is None:
+            self._ortho_im_filename = self._raw_im_filename.parent.joinpath(self._raw_im_filename.stem + '_ORTHO.TIF')
+        else:
+            self._ortho_im_filename = pathlib.Path(ortho_im_filename)
+
+        if config is None: # set defaults:
+            config = dict(dem_interp='cubic_spline', ortho_interp='bilinear', resolution=[0.5, 0.5],
+                          compression='deflate', tile_size=[512, 512])
+
+        self._parse_config(config)
+        self.dem_min = 0.
+        # TODO: some error checking like does DEM cover raw image
+
+    def _parse_config(self, config):
+        """
+        Parse dict config items where necessary
+
+        Parameters
+        ----------
+        config :  dict
+                  e.g. dict(dem_interp='cubic_spline', ortho_interp='bilinear', resolution=[0.5, 0.5],
+                          compression='deflate', tile_size=[512, 512])
+        """
+
+        for key, value in config.items():
+            setattr(self, key, value)
+
+        try:
+            self.dem_interp = Resampling[config['dem_interp']]
+        except :
+            logger.error(f'Unknown dem_interp configuration type: {config["dem_interp"]}')
+            raise
+
+        cv_interp_dict = dict(average=cv2.INTER_AREA, bilinear=cv2.INTER_LINEAR, cubic=cv2.INTER_CUBIC,
+                              lanczos=cv2.INTER_LANCZOS4, nearest=cv2.INTER_NEAREST)
+
+        if self.ortho_interp not in cv_interp_dict:
+            raise Exception(f'Unknown ortho_interp configuration type: {config["ortho_interp"]}')
+        else:
+            self.ortho_interp = cv_interp_dict[self.ortho_interp]
+
+    def _get_dem_min(self):
+        """
+        Find minimum of the DEM over the bounds of the raw image
+        """
+
+        dem_min = 0.
+        with rio.Env():
+            with rio.open(self._raw_im_filename, 'r') as raw_im:
+                with rio.open(self._dem_filename, 'r') as dem_im:
+                    # find raw image bounds in DEM CRS
+                    [dem_xbounds, dem_ybounds] = transform(raw_im.crs, dem_im.crs,
+                                                           [raw_im.bounds.left, raw_im.bounds.right],
+                                                           [raw_im.bounds.top, raw_im.bounds.bottom])
+                    dem_win = rio.windows.from_bounds(dem_xbounds[0], dem_ybounds[1], dem_xbounds[1], dem_ybounds[0],
+                                                      transform=dem_im.transform)
+
+                    # read DEM in raw image ROI and find minimum
+                    dem_im_array = dem_im.read(1, window=dem_win)
+                    dem_min = np.max([dem_im_array.min(), 0])
+
+        return dem_min
+
+    def _get_ortho_bounds(self, dem_min=0):
+        """
+        Get the bounds of the output ortho image in its CRS
+
+        Parameters
+        ----------
+        dem_min : (optional) minimum altitude Z value over the image area in m, default=0
+
+        Returns
+        -------
+        ortho_bl: numpy.array_like
+                  [x, y] co-ordinates of the bottom left corner
+        ortho_tr: numpy.array_like
+                  [x, y] co-ordinates of the top right corner
+        """
+
+        with rio.Env():
+            with rio.open(self._raw_im_filename, 'r') as raw_im:
+                # find the bounds of the ortho by projecting 2D image pixel corners onto 3D Z plane = dem_min
+                ortho_cnrs = self._camera.project_to_z(
+                    np.array([[0, 0], [raw_im.width, 0], [raw_im.width, raw_im.height], [0, raw_im.height]]).T,
+                    dem_min)[:2, :]
+
+                raw_cnrs = np.array(
+                    [[raw_im.bounds.left, raw_im.bounds.bottom], [raw_im.bounds.right, raw_im.bounds.bottom],
+                     [raw_im.bounds.right, raw_im.bounds.top], [raw_im.bounds.left, raw_im.bounds.top]]).T
+
+                ortho_cnrs = np.column_stack([ortho_cnrs, raw_cnrs])  # make doubl sure we encompass the raw image
+                ortho_bl = ortho_cnrs.min(axis=1)   # bottom left
+                ortho_tr = ortho_cnrs.max(axis=1)   # top right
+                # ortho_wh = np.ceil(np.abs((ortho_bl - ortho_tr).squeeze()[:2] / self.resolution)) # ortho im width, height
+                # ortho_transform = rio.transform.from_origin(ortho_bl[0], ortho_tr[1], *self.resolution)
+
+        return ortho_bl, ortho_tr
+
