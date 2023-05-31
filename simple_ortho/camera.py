fix precision problem for high res imagery

simple_ortho/camera.py
@@ -26,7 +26,7 @@ logger = get_logger(__name__)
 
 class Camera:
     def __init__(
-        self, focal_len, sensor_size, im_size, geo_transform, position, orientation, dtype='float32', dist_coeff=None
+        self, focal_len, sensor_size, im_size, geo_transform, position, orientation, dist_coeff=None
     ):
         """
         Camera class to project from 2D camera (i,j) pixel co-ordinates to 3D world (x,y,z) co-ordinates,
@@ -46,11 +46,8 @@ class Camera:
                         column vector of [x=easting, y=northing, z=altitude] camera location co-ordinates, in image CRS
         orientation :   numpy.array_like
                         camera orientation [omega, phi, kappa] angles in radians
-        dtype :         numpy.dtype, Type
-                        Data type to use for camera parameters (to avoid e.g. unproject forcing float32 to 64)
         """
 
-        self._dtype = dtype
         self.update_extrinsic(position, orientation)
 
         if np.size(sensor_size) != 2 or np.size(im_size) != 2:
@@ -80,7 +77,7 @@ class Camera:
         """
         if np.size(position) != 3 or np.size(orientation) != 3:
             raise Exception('len(position) != 3 or len(orientation) != 3')
-        self._T = np.array(position, dtype=self._dtype).reshape(3, 1)
+        self._T = np.array(position).reshape(3, 1)
 
         self._omega, self._phi, self._kappa = orientation
 
@@ -105,7 +102,7 @@ class Camera:
              [0, 0, 1]]
         )
 
-        self._R = np.dot(np.dot(omega_r, phi_r), kappa_r).astype(self._dtype)
+        self._R = np.dot(np.dot(omega_r, phi_r), kappa_r)
         self._Rtv = cv2.Rodrigues(self._R.T)[0]
         return
 
@@ -124,7 +121,7 @@ class Camera:
         self._K = np.array(
             [[-sigma_xy[0], 0, self._im_size[0] / 2],
              [0, sigma_xy[1], self._im_size[1] / 2],
-             [0, 0, 1]], dtype=self._dtype
+             [0, 0, 1]]
         )
         return
 
@@ -152,7 +149,7 @@ class Camera:
 
         if use_cv:  # use opencv
             ij, _ = cv2.projectPoints(
-                x - self._T, self._Rtv, np.array([0., 0., 0.], dtype=self._dtype), self._K, distCoeffs=self._dist_coeff
+                x - self._T, self._Rtv, np.array([0., 0., 0.]), self._K, distCoeffs=self._dist_coeff
             )
             ij = np.squeeze(ij).T
         else:

simple_ortho/command_line.py
@@ -173,7 +173,7 @@ def main(src_im_file, dem_file, pos_ori_file, ortho_dir=None, read_conf=None, wr
                     dist_coeff = camera_config['dist_coeff'] if 'dist_coeff' in camera_config else None
                     camera = Camera(
                         camera_config['focal_len'], camera_config['sensor_size'], im_size, geo_transform, position,
-                        orientation, dtype=np.float32, dist_coeff=dist_coeff
+                        orientation, dist_coeff=dist_coeff
                     )
 
                     # create OrthoIm  and orthorectify

simple_ortho/ortho.py
@@ -263,9 +263,11 @@ class OrthoIm:
                         array of altitude values on corresponding to ortho image i.e. on the same grid
         """
 
-        # initialise tile grid here once off (save cpu) - to offset later (requires N-up geotransform)
-        j_range = np.arange(0, self.tile_size[0], dtype='float32')
-        i_range = np.arange(0, self.tile_size[1], dtype='float32')
+        # Initialise tile grid here once off (save cpu) - to offset later (requires N-up geotransform).
+        # Note that numpy is left to its default float64 precision for building the xy ortho grids in geo-referenced
+        # co-ordinates.  This precision is needed for e.g. high resolution drone imagery.
+        j_range = np.arange(0, self.tile_size[0])
+        i_range = np.arange(0, self.tile_size[1])
         jgrid, igrid = np.meshgrid(j_range, i_range, indexing='xy')
         xgrid, ygrid = ortho_profile['transform'] * [jgrid, igrid]
 
@@ -311,8 +313,10 @@ class OrthoIm:
                         src_ji = self._camera.unproject(
                             np.array([ortho_xgrid.reshape(-1, ), ortho_ygrid.reshape(-1, ), ortho_zgrid.reshape(-1, )])
                         )
-                        src_jj = src_ji[0, :].reshape(ortho_win.height, ortho_win.width)
-                        src_ii = src_ji[1, :].reshape(ortho_win.height, ortho_win.width)
+                        # now that co-rds are in pixel units, they are converted to float32 for compatibility with
+                        # cv2.remap (without meaningful loss of precision).
+                        src_jj = src_ji[0, :].reshape(ortho_win.height, ortho_win.width).astype('float32')
+                        src_ii = src_ji[1, :].reshape(ortho_win.height, ortho_win.width).astype('float32')
 
                         # Interpolate the ortho tile from the source image based on warped/unprojected grids.
                         ortho_im_win_array = np.full(
@@ -419,15 +423,13 @@ class OrthoIm:
                 #  for the batch, so perhaps a separate dem class
                 # TODO: read only the relevant sub-window from the dem, dems may be high res / large memory - does
                 #  passing rio.band(dem_im) do this automatically?
-                # TODO: generalise the dtype, to allow float64 internal computation - see
-                #  https://github.com/leftfield-geospatial/simple-ortho/issues/3
                 # Reproject dem to ortho crs and grid. Use nan for dem nodata internally, so that portions of the
                 # ortho in dem nodata, or outside the dem, will be set to self.nodata in self._remap_src_to_ortho.
-                dem_array = np.full((ortho_wh[1], ortho_wh[0]), fill_value=np.float32('nan'), dtype='float32')
+                dem_array = np.full((ortho_wh[1], ortho_wh[0]), fill_value=np.nan)
                 reproject(
                     rio.band(dem_im, self.dem_band), dem_array, dst_transform=ortho_transform,
                     dst_crs=ortho_profile['crs'], resampling=self.dem_interp, src_transform=dem_im.transform,
-                    src_crs=dem_im.crs, num_threads=multiprocessing.cpu_count(), dst_nodata=np.float32('nan'),
+                    src_crs=dem_im.crs, num_threads=multiprocessing.cpu_count(), dst_nodata=np.nan,
                     init_dest_nodata=True
                 )
 

tests/test_simple_ortho.py
@@ -37,8 +37,7 @@ def create_camera():
                            -0.001324094632423112, 0.4699268845788538)
 
     # create camera
-    return Camera(120, [92.160, 165.888], [640, 1152], transform, position, orientation,
-                               dtype=np.float32)
+    return Camera(120, [92.160, 165.888], [640, 1152], transform, position, orientation)
 
 
 class TestSimpleOrthoModule(unittest.TestCase):
@@ -57,7 +56,7 @@ class TestSimpleOrthoModule(unittest.TestCase):
         # create arbitrary world co-ords and unproject to image co-ords
         start_x = np.array([-52359.614680, -3727390.785280, 500])
         x = (start_x + np.random.rand(100, 3) * np.array([5000, 5000, 500])).T
-        ij = camera.unproject(x, use_cv=False)
+        ij = camera.unproject(x)
 
         # re-project image co-ords to world space at original z
         x2 = camera.project_to_z(ij, x[2, :])