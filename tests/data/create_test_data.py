2e109df1e66dc84f930ad5e367b7fbc86818806d
Dugal Harris <dugalh@gmail.com>
2023-10-06 15:38:55 +0200

    add xyz_opk format csv for odm data and optional dst_crs for downsample_dem

--- tests/data/create_test_data.py
+++ tests/data/create_test_data.py
@@ -1,4 +1,5 @@
 """ Functions to create NGI & ODM test data sets. """
+
 from pathlib import Path
 from typing import Tuple
 import rasterio as rio
@@ -11,6 +12,7 @@ import csv
 import re
 import numpy as np
 import json
+import multiprocessing
 
 ngi_src_root = Path('V:/Data/SimpleOrthoEgs/NGI_3324C_2015_Baviaans/')
 ngi_test_root = Path('C:/Data/Development/Projects/simple-ortho/tests/data/ngi')
@@ -60,12 +62,12 @@ def downsample_rgb(
             dst_im.write(array)
 
 
-def downsample_dem(src_file: Path, dst_file: Path, ds_fact: int = 8, bounds: Tuple = None):
+def downsample_dem(src_file: Path, dst_file: Path, ds_fact: int = 8, bounds: Tuple = None, dst_crs: rio.CRS = None):
     """
     Downsample `src_file` by `ds_fact`, cropping to `bounds` if specified.  Write to float32 deflate geotiff `dst_file`.
     """
     nodata = float('nan')
-    with rio.Env(GDAL_NUM_THREADS='ALL_CPUS', GTIFF_FORCE_RGBA=False), rio.open(src_file, 'r') as src_im:
+    with (rio.Env(GDAL_NUM_THREADS='ALL_CPUS', GTIFF_FORCE_RGBA=False), rio.open(src_file, 'r') as src_im):
         if bounds:
             src_win = expand_window_to_grid(src_im.window(*bounds))
             src_transform = src_im.window_transform(src_win)
@@ -76,16 +78,17 @@ def downsample_dem(src_file: Path, dst_file: Path, ds_fact: int = 8, bounds: Tup
         src_array = src_im.read(window=src_win)
 
         # reproject rather than read(out_shape=...) to ensure square pixels
+        dst_crs = dst_crs or src_im.crs
         dst_array, dst_transform = reproject(
-            src_array, src_crs=src_im.crs, src_transform=src_transform, src_nodata=src_im.nodata, dst_crs=src_im.crs,
+            src_array, src_crs=src_im.crs, src_transform=src_transform, src_nodata=src_im.nodata, dst_crs=dst_crs,
             dst_nodata=nodata, dst_resolution=(np.abs(src_transform.a) * ds_fact, ) * 2, resampling=Resampling.cubic,
-            init_dest_nodata=True
+            init_dest_nodata=True, num_threads=multiprocessing.cpu_count()
         )
         dst_profile = src_im.profile.copy()
         dst_profile.update(
-            width=dst_array.shape[-1], height=dst_array.shape[-2], transform=dst_transform, compress='deflate',
-            interleave='band', tiled=True, blockxsize=256, blockysize=256, dtype='float32', driver='GTiff',
-            predictor=2, zlevel=9, nodata=nodata
+            crs=dst_crs, width=dst_array.shape[-1], height=dst_array.shape[-2], transform=dst_transform,
+            compress='deflate', interleave='band', tiled=True, blockxsize=256, blockysize=256, dtype='float32',
+            driver='GTiff', predictor=2, zlevel=9, nodata=nodata
         )
         with rio.open(dst_file, 'w', **dst_profile) as dst_im:
             dst_im.write(dst_array)
@@ -112,12 +115,14 @@ def create_ngi_test_data():
         downsample_rgb(src_rgb_file, dst_rgb_file, ds_fact=ds_fact, scale_clip=(255 / 3000))
 
     # downsample dem
+    with rio.open(ngi_src_root.joinpath(src_rgb_files[0]), 'r') as src_im:
+        dst_crs = src_im.crs
     src_dem_file = ngi_src_root.joinpath(dem_file)
     dst_dem_file = ngi_test_root.joinpath('dem.tif')
     if dst_dem_file.exists():
         dst_dem_file.unlink()
         dst_dem_file.with_suffix(dst_dem_file.suffix + '.aux.xml').unlink(missing_ok=True)
-    downsample_dem(src_dem_file, dst_dem_file, bounds=None, ds_fact=ds_fact)
+    downsample_dem(src_dem_file, dst_dem_file, bounds=None, ds_fact=ds_fact, dst_crs=dst_crs)
 
     # copy & convert csv exterior params
     src_ext_file = ngi_src_root.joinpath('camera_pos_ori.txt')
@@ -208,6 +213,14 @@ def create_io_test_data():
         writer.writerow(['filename', 'latitude', 'longitude', 'altitude', 'roll', 'pitch', 'yaw', 'camera', 'other'])
         for exif in exif_list:
             writer.writerow([exif.filename.name, *exif.lla, *exif.rpy, cam_id, 'ignored'])
+
+    # create xyz_opk csv file for odm data
+    with open(io_root.joinpath('odm_xyz_opk.csv'), 'w', newline='') as f:
+        writer = csv.writer(f, delimiter=' ', quotechar="'", quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerow(['filename', 'easting', 'northing', 'altitude', 'omega', 'phi', 'kappa', 'camera'])
+        ext_param_dict = osfm_reader.read_ext_param()
+        for filename, ext_param in ext_param_dict.items():
+            writer.writerow([filename, *ext_param['xyz'], *ext_param['opk'], cam_id])
 
     # create xyz_opk csv file for ngi data
     src_csv_file = ngi_test_root.joinpath('camera_pos_ori.txt')
