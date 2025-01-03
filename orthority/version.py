4db98732ca0d841d19780254ca5da9ec7eeeb063
Dugal Harris <dugalh@gmail.com>
2025-01-03 10:43:54 +0200

    increment version

--- orthority/version.py
+++ orthority/version.py
@@ -13,4 +13,4 @@
 # You should have received a copy of the GNU Affero General Public License along with Orthority.
 # If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
