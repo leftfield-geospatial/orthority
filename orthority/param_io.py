offset coordinates in _cv_ext_to_oty_ext() to fix float32 precision issues

orthority/param_io.py
@@ -836,14 +836,20 @@ def _rpy_to_opk(
 
 
 def _cv_ext_to_oty_ext(
-    t: Sequence[float] | np.ndarray, r: Sequence[float] | np.ndarray
+    t: Sequence[float] | np.ndarray,
+    r: Sequence[float] | np.ndarray,
+    ref_xyz: Sequence[float] | np.ndarray | None = None,
 ) -> tuple[tuple[float, float, float], tuple[float, float, float]]:
     """Convert OpenCV / OpenSfM rotation and translation vectors to Orthority format and
-    convention camera (x, y, z) position and (omega, phi, kappa) angles.
+    convention camera (x, y, z) position and (omega, phi, kappa) angles.  Camera positions are
+    offset by ``ref_xyz`` if it is supplied.
     """
     # adapted from ODM: https://github.com/OpenDroneMap/ODM/blob/master/opendm/shots.py
     R = cv2.Rodrigues(np.array(r))[0].T
-    xyz = tuple((-R.dot(t)).squeeze().tolist())
+    xyz = (-R.dot(t)).squeeze()
+    if ref_xyz is not None:
+        xyz += ref_xyz
+    xyz = tuple(xyz.tolist())
     # rotate camera coords from OpenSfM / OpenCV to PATB convention
     R_ = R.dot(np.array([[1, 0, 0], [0, -1, 0], [0, 0, -1]]))
     opk = _rotation_to_opk(R_)
@@ -1247,8 +1253,9 @@ class OsfmReader(FrameReader):
         ext_param_dict = {}
         for filename, shot_dict in self._json_dict['shots'].items():
             # convert reconstruction 'translation' and 'rotation' to oty exterior params
-            delta_xyz, opk = _cv_ext_to_oty_ext(shot_dict['translation'], shot_dict['rotation'])
-            xyz = tuple((np.array(ref_xyz) + np.array(delta_xyz)).tolist())
+            xyz, opk = _cv_ext_to_oty_ext(
+                shot_dict['translation'], shot_dict['rotation'], ref_xyz=ref_xyz
+            )
             cam_id = shot_dict['camera']
             cam_id = cam_id[3:] if cam_id.startswith('v2 ') else cam_id
             ext_param_dict[filename] = dict(xyz=xyz, opk=opk, camera=cam_id)