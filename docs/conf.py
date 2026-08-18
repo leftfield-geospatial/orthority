95105713193bb5c014f05387ebe749d769c2032a
Dugal Harris <dugalh@gmail.com>
2026-08-18 19:23:07 +0200

    tidy up

--- docs/conf.py
+++ docs/conf.py
@@ -3,24 +3,15 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path information -----------------------------------------------------
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath('..'))
+# -- Project information -----------------------------------------------------
 from orthority.version import __version__
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Orthority'
-copyright = '2023, Leftfield Geospatial'
+copyright = 'Leftfield Geospatial'
 author = 'Leftfield Geospatial'
 release = __version__
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.autodoc',
@@ -30,19 +21,12 @@ extensions = [
     'sphinx_copybutton',
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-# nitpicky = True
+exclude_patterns = ['_build', 'scripts']
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'furo'
-# html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
 # -- Options for autodoc -----------------------------------------------------
-# see https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
 autodoc_member_order = 'bysource'
 autodoc_typehints = 'description'
 
@@ -60,6 +44,4 @@ intersphinx_mapping = {
 highlight_language = 'none'
 
 # -- Options for autosectionlabel ----------------------------------------------------
-# Make sure the target is unique
 autosectionlabel_prefix_document = True
-# autosectionlabel_maxdepth = 3  # avoid duplicate section labels for CLI examples
